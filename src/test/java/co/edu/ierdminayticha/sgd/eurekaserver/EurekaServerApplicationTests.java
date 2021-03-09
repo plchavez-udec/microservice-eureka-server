f765c42d005c13bd3b07b794a6bcda331f845138
Pedro Luis Chavez Castro <plchavez@ucundinamarca.edu.co>
2021-03-09 16:40:04 -0500

    Primer commit microservice-eureka-server

---
+++ src/test/java/co/edu/ierdminayticha/sgd/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ierdminayticha.sgd.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
